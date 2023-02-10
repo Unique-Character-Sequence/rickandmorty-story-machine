d9e3fc761a91dcfe37642f581792c1c7a843865b
Unique-Character-Sequence <76056880+Unique-Character-Sequence@users.noreply.github.com>
2023-02-10 23:21:37 +0200

    [+ Функционал] NavigationBar с картинкой-кнопкой

--- src/views/MainPage.jsx
+++ src/views/MainPage.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import RaM_portal_2 from '../assets/RaM_portal_2_cut.png'
+import RaM_portal_2 from '../assets/RaM_API_logo.png'
 import {Link as RouterLink} from "react-router-dom";
 import Button from "@mui/material/Button";
 import Link from "@mui/material/Link";
 import Container from "@mui/material/Container";
 import Grid from '@mui/material/Unstable_Grid2';
 import Box from "@mui/material/Box";
-import { useTheme } from '@mui/material/styles';
+import {useTheme} from '@mui/material/styles';
+import NavigationBar from "./NavigationBar";
 
 const styleCenter = {display: "flex", justifyContent: "center"};
 
@@ -15,9 +16,7 @@ const MainPage = () => {
     return (
         <Grid container spacing={2}>
             <Grid xs={12}>
-                <Box sx={{...styleCenter, backgroundColor: "#00A0FF"}}>
-                    <img style={{width: "40%", maxWidth:"400px", imageRendering: "-webkit-optimize-contrast"}} src={RaM_portal_2} alt=""/>
-                </Box>
+                <NavigationBar style={styleCenter}/>
             </Grid>
             <Grid xs={12}>
                 <Box sx={{...styleCenter, backgroundColor: "#118CAF", padding: useTheme().spacing(1)}}>
---
+++ src/views/NavigationBar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Link as RouterLink} from "react-router-dom";
+import RaM_portal_2 from "../assets/RaM_API_logo.png";
+import Box from "@mui/material/Box";
+
+// Я решил вложить стиль прямо сюда, а не передавать
+const styleCenter = {display: "flex", justifyContent: "center"};
+
+const NavigationBar = () => {
+    return (
+        <Box sx={{...styleCenter, backgroundColor: "#00A0FF"}}>
+            <RouterLink style={{width: "40%", ...styleCenter}} to="/">
+                <img
+                    style={{maxWidth: "70px", imageRendering: "-webkit-optimize-contrast"}}
+                    src={RaM_portal_2} alt=""/>
+            </RouterLink>
+        </Box>
+    );
+};
+
+export default NavigationBar;
