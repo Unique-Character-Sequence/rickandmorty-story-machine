5f54b912f14992ddc396057219b90c32c1c9958c
Unique-Character-Sequence <76056880+Unique-Character-Sequence@users.noreply.github.com>
2023-02-12 21:58:28 +0200

    [Функционал] Делаю запрос на API

--- src/views/MainPage.jsx
+++ src/views/MainPage.jsx
@@ -8,22 +8,48 @@ import Grid from '@mui/material/Unstable_Grid2';
 import Box from "@mui/material/Box";
 import {useTheme} from '@mui/material/styles';
 import NavigationBar from "./NavigationBar";
+import axios from "axios";
+import {UsePaginationItem as log} from "@mui/material/usePagination/usePagination";
 
-const styleCenter = {display: "flex", justifyContent: "center"};
+const API_URL = "https://rickandmortyapi.com/api"
+
+function randomNumberInRange(min, max) {
+    // 👇️ get number between min (inclusive) and max (inclusive)
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+const getAPICharactersArray = () => {
+    const randomNum = randomNumberInRange(1, 826)
+    axios.get(`${API_URL}/character/${randomNum}`)
+        .then(response => {
+            // Handle the successful response
+            console.log(response.data);
+        })
+        .catch(error => {
+            // Handle the error
+            console.error(error);
+        });
+}
+
 
 
 const MainPage = () => {
+    const styleCenter = {display: "flex", justifyContent: "center", padding: useTheme().spacing()};
     return (
-        <Grid container spacing={2}>
+        <Grid container spacing={1}>
             <Grid xs={12}>
                 <NavigationBar style={styleCenter}/>
             </Grid>
             <Grid xs={12}>
-                <Box sx={{...styleCenter, backgroundColor: "#118CAF", padding: useTheme().spacing(1)}}>
-                    <p>
+                <Box sx={{...styleCenter, backgroundColor: "#118CAF"}}>
                         Pick Your Character: <br/>
-                        Choosing between them
-                    </p>
+                        Choosing between them <br/>
+                </Box>
+            </Grid>
+            <Grid xs={12}>
+                <Box sx={{...styleCenter, backgroundColor: "#B6BCDE"}}>
+                    <Button onClick={getAPICharactersArray} variant="outlined">Generate</Button>
                 </Box>
             </Grid>
             <Grid xs={12}>
--- src/views/NavigationBar.js
+++ src/views/NavigationBar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Link as RouterLink} from "react-router-dom";
 import RaM_portal_2 from "../assets/RaM_API_logo.png";
 import Box from "@mui/material/Box";
+import useTheme from "@mui/material/styles/useTheme";
 
 // Я решил вложить стиль прямо сюда, а не передавать
 const styleCenter = {display: "flex", justifyContent: "center"};
@@ -11,7 +12,7 @@ const NavigationBar = () => {
         <Box sx={{...styleCenter, backgroundColor: "#00A0FF"}}>
             <RouterLink style={styleCenter} to="/">
                 <img
-                    style={{maxWidth: "70px", imageRendering: "-webkit-optimize-contrast"}}
+                    style={{maxWidth: "70px", padding: useTheme().spacing(0.5)}}
                     src={RaM_portal_2} alt=""/>
             </RouterLink>
         </Box>
