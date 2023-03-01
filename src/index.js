5b6036961448a501ebf9e940e530787371673db1
Unique-Character-Sequence <76056880+Unique-Character-Sequence@users.noreply.github.com>
2023-03-01 22:39:07 +0200

    [Библиотеки] rtk и react-redux [Фичи] rtk store создан

--- src/index.js
+++ src/index.js
@@ -5,16 +5,20 @@ import {ThemeProvider} from '@mui/material/styles';
 import App from './App';
 import theme from './views/theme';
 import {BrowserRouter} from "react-router-dom";
+import {store} from "./redux/store";
+import {Provider} from "react-redux";
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
-    <BrowserRouter>
-        <ThemeProvider theme={theme}>
-            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-            <CssBaseline/>
-            <App/>
-        </ThemeProvider>
-    </BrowserRouter>,
+    <Provider store={store}>
+        <BrowserRouter>
+            <ThemeProvider theme={theme}>
+                {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+                <CssBaseline/>
+                <App/>
+            </ThemeProvider>
+        </BrowserRouter>
+    </Provider>,
 );
