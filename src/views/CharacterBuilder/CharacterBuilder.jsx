d77ce05d1d518b383f0f1653703ba35752df603e
Pilici Rustam <paroli7328129ad@gmail.com>
2023-05-24 13:45:27 +0300

    Стилизованные карточки без предназначения

--- src/views/CharacterBuilder/CharacterBuilder.jsx
+++ src/views/CharacterBuilder/CharacterBuilder.jsx
@@ -3,20 +3,31 @@ import Container from "@mui/material/Container";
 import {CharacterCreator_Container} from "../CharacterCreator/CharacterCreator_Container";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import {useSelector} from "react-redux";
+import CharacterBonusCard from "./CharacterBonusCard";
+import Stack from "@mui/material/Stack";
 
-const CharacterBuilder = props => (
-    <div>
-        <Container maxWidth="lg" sx={{display: 'flex', backgroundColor: 'rgba(255, 255, 255, 0.4)'}}>
-            <Box sx={{flex: 1}}>
-                <img src="https://rickandmortyapi.com/api/character/avatar/5.jpeg" alt=""/>
-                <Typography variant="h6">Allocate points</Typography>
-                <CharacterCreator_Container/>
-            </Box>
-            <Box sx={{flex: 1}}>
-
-            </Box>
-        </Container>
-    </div>
-);
+const CharacterBuilder = props => {
+    const characterPicture = useSelector((state) => state.character.characterObj.imageUrl)
+    return (
+        <div>
+            <Container maxWidth="lg" sx={{display: 'flex', backgroundColor: 'rgba(0, 0, 0, 0.1)'}}>
+                <Box sx={{flex: 1}}>
+                    <img src={characterPicture} alt=""/>
+                    <Typography variant="h6">Allocate points</Typography>
+                    <CharacterCreator_Container/>
+                </Box>
+                <Box sx={{flex: 1}}>
+                    <Stack spacing={2}>
+                        <CharacterBonusCard characterPicture={characterPicture}/>
+                        <CharacterBonusCard characterPicture={characterPicture}/>
+                        <CharacterBonusCard characterPicture={characterPicture}/>
+                        <CharacterBonusCard characterPicture={characterPicture}/>
+                    </Stack>
+                </Box>
+            </Container>
+        </div>
+    );
+};
 
 export default CharacterBuilder;
