dbfb9b0907f07565e605113cea0f2908a9eb7e39
Pilici Rustam <paroli7328129ad@gmail.com>
2023-05-27 14:36:38 +0300

    [Фичи] Кнопка Back переключает stage [Стили] Кнопки Back и Next

--- src/views/CharacterBuilder/CharacterBuilder.jsx
+++ src/views/CharacterBuilder/CharacterBuilder.jsx
@@ -3,12 +3,23 @@ import Container from "@mui/material/Container";
 import {CharacterCreator_Container} from "../CharacterCreator/CharacterCreator_Container";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import CharacterBonusCard from "./CharacterBonusCard";
 import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import styles from "./CharacterBuilder.module.css";
+import {Link as RouterLink} from "react-router-dom";
+import {setStage} from "../../redux/mainPage_Slice";
 
 const CharacterBuilder = props => {
     const characterPicture = useSelector((state) => state.character.characterObj.imageUrl)
+
+    const dispatch = useDispatch()
+
+    const handleBackClick = (dispatch) => {
+        dispatch(setStage(1))
+    };
+
     return (
         <div>
             <Container maxWidth="lg" sx={{display: 'flex', backgroundColor: 'rgba(0, 0, 0, 0.1)'}}>
@@ -24,6 +35,16 @@ const CharacterBuilder = props => {
                         <CharacterBonusCard characterPicture={characterPicture}/>
                         <CharacterBonusCard characterPicture={characterPicture}/>
                     </Stack>
+                    <div className={styles.buttonsContainer}>
+                        <Button className={styles.button_Back} variant="contained"
+                                onClick={() => {
+                                    handleBackClick(dispatch)
+                                }}
+                        >Back</Button>
+                        <RouterLink to="/app">
+                            <Button className={styles.button_Next} variant="contained">Next</Button>
+                        </RouterLink>
+                    </div>
                 </Box>
             </Container>
         </div>
